f3b7e83e7f06d0e1227655afb568e0766b626fb8
Marcus Novak <marcusanovak@gmail.com>
2014-11-26 15:02:23 -0600

    Add Shuffle and Print functions to Deck script

--- Assets/DeckOfCards/Scripts/Deck.cs
+++ Assets/DeckOfCards/Scripts/Deck.cs
@@ -17,9 +17,13 @@ public class Deck : MonoBehaviour
         _cards = new List<Card>();
 
         Initialize();
+        Shuffle();
     }
 
 
+    /// <summary>
+    /// Create cards and initialize everything
+    /// </summary>
     private void Initialize()
     {
         foreach (Value value in System.Enum.GetValues(typeof(Value)))
@@ -35,6 +39,43 @@ public class Deck : MonoBehaviour
                 card.gameObject.name = card.Name;
             }
         }
+    }
+
+
+    /// <summary>
+    /// Shuffle the deck
+    /// </summary>
+    public void Shuffle()
+    {
+        if (_cards != null && _cards.Count > 1)
+        {
+            // Unparent all cards so we can parent them back to the deck how we want
+            foreach (Card card in _cards)
+                card.transform.parent = null;
+
+            System.Random random = new System.Random();
+
+            // Swap cards around randomly
+            for (int i = _cards.Count - 1; i >= 0; i--)
+            {
+                int randIndex = random.Next(i + 1);
+
+                Card tmp = _cards[i];
+                _cards[i] = _cards[randIndex];
+                _cards[randIndex] = tmp;
+                _cards[randIndex].transform.parent = this.transform;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Print the list of cards
+    /// </summary>
+    public void PrintDeck()
+    {
+        foreach (Card card in _cards)
+            Debug.Log(card.Name);
     }
 
 
